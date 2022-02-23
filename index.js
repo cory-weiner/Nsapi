4a566f5efe16dbba470f8237cfd0d07b6c9f4196
Cory Weiner <corymw@gmail.com>
2022-02-23 14:04:31 -0600

    error handling

--- index.js
+++ index.js
@@ -158,15 +158,15 @@ class Nsapi{
 
               console.log(config)
         
-              return axios(config)
-              .then(function (response) {
-                return response.data
+            return new Promise((resolve,reject)=>{
+                return axios(config).then(function (response) {
+                    console.log("response")
+                    resolve(response.data)
+                }).catch(error=>{
+                    console.log(error)
+                    reject(error)
+                })
               })
-              .catch(function (error) {
-                console.log(error.response.data.error);
-                throw error
-                // return error
-              });
     
      
 
